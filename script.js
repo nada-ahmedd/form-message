5d657a667a417696f52b2b2aabad4c17318a1415
nada-ahmedd <nada20-02188@student.eelu.edu.eg>
2024-12-25 08:44:38 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,38 @@
+function addMessage() {
+    const content = document.getElementById("content").value.trim();
+    const status = document.getElementById("status").value;
+    const messages = document.getElementById("messages");
+    const errorMessage = document.getElementById("error-message");
+
+    if (content === "") {
+        errorMessage.textContent = "Please Enter Your Content";
+        return;
+    }
+    errorMessage.textContent = "";
+    const container = document.createElement("div");
+    container.className = `status-container`;
+    container.innerHTML = `
+        <div class="status-message ${status === "success" ? "green" : "red"}">${content}</div>
+        <div class="progress-bar"><div class="progress-fill"></div></div>`;
+
+    messages.appendChild(container);
+
+    const progressFill = container.querySelector(".progress-fill");
+    let progress = 0, isPaused = false;
+
+   function updateProgress() {
+        progressFill.style.width = `${progress}%`;
+        if (progress >= 100) {
+            clearInterval(interval);
+            container.remove();
+        }
+    }
+     container.addEventListener("mouseenter", () => { isPaused = true; });
+    container.addEventListener("mouseleave", () => { isPaused = false; });
+      const interval = setInterval(() => {
+        if (!isPaused) {
+            progress += 1;
+            updateProgress();
+        }
+    }, 50);
+}
